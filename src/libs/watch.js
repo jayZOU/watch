2438c8c8e5e3aeaa858f34d500625b928764f49c
akiq2016 <akiq2016@gmail.com>
2018-05-29 19:18:07 +0800

    tweaks

--- src/libs/watch.js
+++ src/libs/watch.js
@@ -1,105 +1,106 @@
 class Watch {
-	constructor (context) {
-		this.page = context
-		this.observers = new Map()
-		this.init()
-	}
+  constructor (context) {
+    this.page = context
+    this.observers = new Map()
+    this.init()
+  }
 
-	// 将 watch 数据写入观察者队列
-	init() {
-		const ctx = this.page
+  // 将 watch 数据写入观察者队列
+  init() {
+    const ctx = this.page
 
-		Object.keys(ctx.watch).forEach(path => {
-			if (this.inData(path)) {
-				this.observers.set(path, ctx.watch[path])
-			}
-		})
-	}
+    Object.keys(ctx.watch).forEach(path => {
+      if (this.inData(path)) {
+        this.observers.set(path, ctx.watch[path])
+      }
+    })
+  }
 
-	// 更新值并触发回调 不更新视图
-	data(path, newVal) {
-		if (this.inData(path)) {
-			this.setter(this.page.data, path, newVal)
-		}
-	}
+  // 更新值并触发回调 不更新视图
+  data(path, newVal) {
+    if (this.inData(path)) {
+      this.setter(this.page.data, path, newVal)
+    }
+  }
 
-	// 更新值并触发回调 并更新视图
-	setData(obj) {
-		const oldData = Object.assign({}, this.page.data)
+  // 更新值并触发回调 并更新视图
+  setData(obj) {
+    const oldData = Object.assign({}, this.page.data)
 
-		this.page.setData(obj)
-		Object.keys(obj).forEach(path => {
-			if (this.observers.has(path)) {
-				this.notify(path, obj[path], this.getter(oldData, path))
-			}
-		})
-	}
+    this.page.setData(obj)
+    Object.keys(obj).forEach(path => {
+      if (this.observers.has(path)) {
+        this.notify(path, obj[path], this.getter(oldData, path))
+      }
+    })
+  }
 
-	// 触发key的对应回调
-	notify(key, newVal, oldVal) {
-		if (!this.observers.has(key)) return
-		this.observers.get(key).call(this.page, newVal, oldVal)
-	}
+  // 触发key的对应回调
+  notify(key, newVal, oldVal) {
+    if (!this.observers.has(key)) return
+    this.observers.get(key).call(this.page, newVal, oldVal)
+  }
 
-	// 删除观察者
-	unSubscribe(key) {
-		this.observers.delete(key)
-	}
+  // 删除观察者
+  unSubscribe(key) {
+    this.observers.delete(key)
+  }
 
-	/**
-	 * 根据路径设置 data 值
-	 * @param {string} path 
-	 * @param {any} newVal 
-	 */
-	setter(data, path, newVal) {
-		if (!this.inData(path)) return
+  /**
+   * 根据路径设置 data 值
+   * @param {string} path 
+   * @param {any} newVal 
+   */
+  setter(data, path, newVal) {
+    if (!this.inData(path)) return
 
-		let oldVal
+    let oldVal
+    const pathArr = this._getPathArr(path)
+    return pathArr.reduce((res, currentPath, currentIndex) => {
+      // assign the new value to the key
+      if (currentIndex === pathArr.length - 1) {
+        oldVal = res[currentPath]
+        res[currentPath] = newVal
+      }
 
-		const REG_KEY = /\[((?:\S+?))\]|\./g
-		const pathArr = path.toString().split(REG_KEY).filter(item => !!item)
-		return pathArr.reduce((res, currentPath, currentIndex) => {
-			// assign the new value to the key
-			if (currentIndex === pathArr.length - 1) {
-				oldVal = res[currentPath]
-				res[currentPath] = newVal
-			}
+      return res[currentPath]
+    }, data)
 
-			return res[currentPath]
-		}, data)
+    if (this.observers.has(path)) {
+      this.notify(path, newVal, oldVal)
+    }
+  }
 
-		if (this.observers.has(path)) {
-			this.notify(path, newVal, oldVal)
-		}
-	}
+  /**
+   * getter 根据路径获取对应 key 的 value
+   * 
+   * @param {object} data data 对象
+   * @param {string} path data 对象下的某一键值对的索引
+   * @return {any} keyValue
+   * 
+   * e.g.
+   * var data = {"a":{"b":{"c":{"d":123}}}}
+   * getter(data, 'a.b[c]') // return {"d":123}
+   */
+  getter(data, path) {
+    const pathArr = this._getPathArr(path)
+    return pathArr.reduce((res, currentPath) => {
+      const currentValueType = Object.prototype.toString.call(res)
+      return /String|Number|Boolean|Null|Undefined/.test(currentValueType)
+        ? undefined
+        : res[currentPath]
+    }, data)
+  }
 
-	/**
-	 * getter 根据路径获取对应 key 的 value
-	 * 
-	 * @param {object} data data 对象
-	 * @param {string} path data 对象下的某一键值对的索引
-	 * @return {any} keyValue
-	 * 
-	 * e.g.
-	 * var data = {"a":{"b":{"c":{"d":123}}}}
-	 * getter(data, 'a.b[c]') // return {"d":123}
-	 */
-	getter(data, path) {
-		const REG_KEY = /\[((?:\S+?))\]|\./g
-		const pathArr = path.toString().split(REG_KEY).filter(item => !!item)
+  // check if data.path exist
+  inData(path) {
+    return this.getter(this.page.data, path) !== undefined ? true : false
+  }
 
-		return pathArr.reduce((res, currentPath) => {
-			const currentValueType = Object.prototype.toString.call(res)
-			return /String|Number|Boolean|Null|Undefined/.test(currentValueType)
-				? undefined
-				: res[currentPath]
-		}, data)
-	}
-
-	// check if data.path exist
-	inData(path) {
-		return this.getter(this.page.data, path) !== undefined ? true : false
-	}
+  _getPathArr(path) {
+    const REG_KEY = /\[((?:\S+?))\]|\./g
+    return path.toString().split(REG_KEY).filter(item => !!item)
+  }
 }
 
 export default Watch
--- test/index.js
+++ test/index.js
@@ -2,172 +2,171 @@ import { expect } from 'chai';
 import Watch from '../src/libs/watch';
 
 let pageCtx = {
-	data: {
-		test: 'abc',
-		a: '1',
-		b: {
-			c: {
-				d: 33
-			},
-			e: [1, 2, [3, 4]]
-		},
-		c: [1, 2, 3],
-		d: 'abc',
-		e: {
-			a: 'efg'
-		},
-		f: {
-			'c.d': {
-				sd: 233
-			},
-			'sa^*sf': 'exm',
-		},
-	},
-	watch: {
-		a: function(val) {
-			console.log('new: %s, old: %s', val);
-		},
-		'b.c.d': function(val, oldVal) {
-			console.log('new: %s, old: %s', val, oldVal);
-		},
-		// 'b.e[2][0]': function(val, oldVal) {
-		//     console.log('new: %s, old: %s', val, oldVal);
-		//     // return [val, oldVal];
-		// },
-		// c: function(val, oldVal) {
-		//     console.log('new: %s, old: %s', val, oldVal);
-		// },
-		// 'b.c.e': function(val, oldVal) {
-		//     console.log('new: %s, old: %s', val, oldVal);
-		// },
-	},
-	setData: function(obj){
-		//setData
-
-	}
+  data: {
+    test: 'abc',
+    a: '1',
+    b: {
+      c: {
+        d: 33
+      },
+      e: [1, 2, [3, 4]]
+    },
+    c: [1, 2, 3],
+    d: 'abc',
+    e: {
+      a: 'efg'
+    },
+    f: {
+      'c.d': {
+        sd: 233
+      },
+      'sa^*sf': 'exm',
+    },
+  },
+  watch: {
+    a: function(val) {
+      console.log('new: %s, old: %s', val);
+    },
+    'b.c.d': function(val, oldVal) {
+      console.log('new: %s, old: %s', val, oldVal);
+    },
+    // 'b.e[2][0]': function(val, oldVal) {
+    //     console.log('new: %s, old: %s', val, oldVal);
+    //     // return [val, oldVal];
+    // },
+    // c: function(val, oldVal) {
+    //     console.log('new: %s, old: %s', val, oldVal);
+    // },
+    // 'b.c.e': function(val, oldVal) {
+    //     console.log('new: %s, old: %s', val, oldVal);
+    // },
+  },
+  setData: function(obj){
+    // setData
+  }
 }
 
 let watch = new Watch(pageCtx);
 
 describe('Correct data', () => {
-	describe('getter test', () => {
-		it('path equal "test" should return "abc"', () => {
-			expect(watch.getter(pageCtx.data, 'test')).to.equal('abc');
-		})
-		it('path equal "a" should return "1"', () => {
-			expect(watch.getter(pageCtx.data, 'a')).to.equal('1');
-		})
-		it('path equal "b.c.d" should return 33', () => {
-			expect(watch.getter(pageCtx.data, 'b.c.d')).to.equal(33);
-		})
-		it('path equal "b.e[2][0]" should return 3', () => {
-			expect(watch.getter(pageCtx.data, 'b.e[2][0]')).to.equal(3);
-		})
-		it('path equal "c[1]" should return 2', () => {
-			expect(watch.getter(pageCtx.data, 'c[1]')).to.equal(2);
-		})
-		it(`path equal "b[c].d" should return 33`, () => {
-			expect(watch.getter(pageCtx.data, "b[c].d")).to.equal(33);
-		})
-		it(`path equal "f[c.d].sd" should return 233`, () => {
-			expect(watch.getter(pageCtx.data, "f[c.d].sd")).to.equal(233);
-		})
-		it(`path equal "f[sa^*sf]" should return exm`, () => {
-			expect(watch.getter(pageCtx.data, "f[sa^*sf]")).to.equal('exm');
-		})
-	})
+  describe('getter test', () => {
+    it('path equal "test" should return "abc"', () => {
+      expect(watch.getter(pageCtx.data, 'test')).to.equal('abc');
+    })
+    it('path equal "a" should return "1"', () => {
+      expect(watch.getter(pageCtx.data, 'a')).to.equal('1');
+    })
+    it('path equal "b.c.d" should return 33', () => {
+      expect(watch.getter(pageCtx.data, 'b.c.d')).to.equal(33);
+    })
+    it('path equal "b.e[2][0]" should return 3', () => {
+      expect(watch.getter(pageCtx.data, 'b.e[2][0]')).to.equal(3);
+    })
+    it('path equal "c[1]" should return 2', () => {
+      expect(watch.getter(pageCtx.data, 'c[1]')).to.equal(2);
+    })
+    it(`path equal "b[c].d" should return 33`, () => {
+      expect(watch.getter(pageCtx.data, "b[c].d")).to.equal(33);
+    })
+    it(`path equal "f[c.d].sd" should return 233`, () => {
+      expect(watch.getter(pageCtx.data, "f[c.d].sd")).to.equal(233);
+    })
+    it(`path equal "f[sa^*sf]" should return exm`, () => {
+      expect(watch.getter(pageCtx.data, "f[sa^*sf]")).to.equal('exm');
+    })
+  })
 
-	describe('setter test', () => {
-		it('path equal "a" should equal data.a', () => {
-			expect(watch.setter(pageCtx.data, 'a', 123)).to.equal(pageCtx.data.a);
-		})
-		it('path equal "b.c.d" should equal data.b.c.d', () => {
-			expect(watch.setter(pageCtx.data, 'b.c.d', 33)).to.equal(33);
-		})
-		it('path equal "b.e[2][0]" should equal data.b.e[2][0]', () => {
-			expect(watch.setter(pageCtx.data, 'b.e[2][0]', 3)).to.equal(3);
-		})
-		it('path equal "c[1]" should equal data.c[1]', () => {
-			expect(watch.setter(pageCtx.data, 'c[1]', 2)).to.equal(2);
-		})
-		it('path equal "f[c.d].sd" should equal data.f[c.d].sd', () => {
-			expect(watch.setter(pageCtx.data, 'f[c.d].sd', 244)).to.equal(244);
-		})
-		it('path equal "f[sa^*sf]" should equal data.f[sa^*sf]', () => {
-			expect(watch.setter(pageCtx.data, 'f[sa^*sf]', 'exm1')).to.equal('exm1');
-		})
-	})
+  describe('setter test', () => {
+    it('path equal "a" should equal data.a', () => {
+      expect(watch.setter(pageCtx.data, 'a', 123)).to.equal(pageCtx.data.a);
+    })
+    it('path equal "b.c.d" should equal data.b.c.d', () => {
+      expect(watch.setter(pageCtx.data, 'b.c.d', 33)).to.equal(33);
+    })
+    it('path equal "b.e[2][0]" should equal data.b.e[2][0]', () => {
+      expect(watch.setter(pageCtx.data, 'b.e[2][0]', 3)).to.equal(3);
+    })
+    it('path equal "c[1]" should equal data.c[1]', () => {
+      expect(watch.setter(pageCtx.data, 'c[1]', 2)).to.equal(2);
+    })
+    it('path equal "f[c.d].sd" should equal data.f[c.d].sd', () => {
+      expect(watch.setter(pageCtx.data, 'f[c.d].sd', 244)).to.equal(244);
+    })
+    it('path equal "f[sa^*sf]" should equal data.f[sa^*sf]', () => {
+      expect(watch.setter(pageCtx.data, 'f[sa^*sf]', 'exm1')).to.equal('exm1');
+    })
+  })
 
-	describe('inData test', () => {
-		it('path equal "a" should return true', () => {
-			expect(watch.inData('a')).to.be.true;
-		})
-		it('path equal "b.c.d" should return true', () => {
-			expect(watch.inData('b.c.d')).to.be.true;
-		})
-		it('path equal "b.e[2][0]" should return true', () => {
-			expect(watch.inData('b.e[2][0]')).to.be.true;
-		})
-		it('path equal "c[1]" should return true', () => {
-			expect(watch.inData('c[1]')).to.be.true;
-		})
-		it('path equal "f[c.d].sd" should return true', () => {
-			expect(watch.inData('f[c.d].sd')).to.be.true;
-		})
-		it('path equal "f[sa^*sf]" should return true', () => {
-			expect(watch.inData('f[sa^*sf]')).to.be.true;
-		})
-	})
+  describe('inData test', () => {
+    it('path equal "a" should return true', () => {
+      expect(watch.inData('a')).to.be.true;
+    })
+    it('path equal "b.c.d" should return true', () => {
+      expect(watch.inData('b.c.d')).to.be.true;
+    })
+    it('path equal "b.e[2][0]" should return true', () => {
+      expect(watch.inData('b.e[2][0]')).to.be.true;
+    })
+    it('path equal "c[1]" should return true', () => {
+      expect(watch.inData('c[1]')).to.be.true;
+    })
+    it('path equal "f[c.d].sd" should return true', () => {
+      expect(watch.inData('f[c.d].sd')).to.be.true;
+    })
+    it('path equal "f[sa^*sf]" should return true', () => {
+      expect(watch.inData('f[sa^*sf]')).to.be.true;
+    })
+  })
 })
 
 describe('Incorrect data', () => {
-	describe('getter test', () => {
-		it('path equal "no" should return undefined', () => {
-			expect(watch.getter(pageCtx.data, 'no')).to.be.undefined;
-		})
-		it('path equal "b.c.a" should return undefined', () => {
-			expect(watch.getter(pageCtx.data, 'b.c.a')).to.be.undefined;
-		})
-		it('path equal "b.e[2][4]" should return undefined', () => {
-			expect(watch.getter(pageCtx.data, 'b.e[2][4]')).to.be.undefined;
-		})
-		it('path equal "b.e[3][4]" should return undefined', () => {
-			expect(watch.getter(pageCtx.data, 'b.e[3][4]')).to.be.undefined;
-		})
-		it('path equal "c[4]" should return undefined', () => {
-			expect(watch.getter(pageCtx.data, 'c[4]')).to.be.undefined;
-		})
-	})
+  describe('getter test', () => {
+    it('path equal "no" should return undefined', () => {
+      expect(watch.getter(pageCtx.data, 'no')).to.be.undefined;
+    })
+    it('path equal "b.c.a" should return undefined', () => {
+      expect(watch.getter(pageCtx.data, 'b.c.a')).to.be.undefined;
+    })
+    it('path equal "b.e[2][4]" should return undefined', () => {
+      expect(watch.getter(pageCtx.data, 'b.e[2][4]')).to.be.undefined;
+    })
+    it('path equal "b.e[3][4]" should return undefined', () => {
+      expect(watch.getter(pageCtx.data, 'b.e[3][4]')).to.be.undefined;
+    })
+    it('path equal "c[4]" should return undefined', () => {
+      expect(watch.getter(pageCtx.data, 'c[4]')).to.be.undefined;
+    })
+  })
 
-	describe('setter test', () => {
-		it('path equal "no" should return undefined', () => {
-			expect(watch.setter(pageCtx.data, 'no', 123)).to.be.undefined;
-		})
-		it('path equal "b.c.a" should return undefined', () => {
-			expect(watch.setter(pageCtx.data, 'b.c.a', 33)).to.be.undefined;
-		})
-		it('path equal "b.e[2][4]" should return undefined', () => {
-			expect(watch.setter(pageCtx.data, 'b.e[2][4]', 3)).to.be.undefined;
-		})
-		it('path equal "c[4]" should return undefined', () => {
-			expect(watch.setter(pageCtx.data, 'c[4]', 2)).to.be.undefined;
-		})
-	})
+  describe('setter test', () => {
+    it('path equal "no" should return undefined', () => {
+      expect(watch.setter(pageCtx.data, 'no', 123)).to.be.undefined;
+    })
+    it('path equal "b.c.a" should return undefined', () => {
+      expect(watch.setter(pageCtx.data, 'b.c.a', 33)).to.be.undefined;
+    })
+    it('path equal "b.e[2][4]" should return undefined', () => {
+      expect(watch.setter(pageCtx.data, 'b.e[2][4]', 3)).to.be.undefined;
+    })
+    it('path equal "c[4]" should return undefined', () => {
+      expect(watch.setter(pageCtx.data, 'c[4]', 2)).to.be.undefined;
+    })
+  })
 
 
-	describe('inData test', () => {
-		it('path equal "no" should return true', () => {
-			expect(watch.inData('no')).to.be.false;
-		})
-		it('path equal "b.c.a" should return true', () => {
-			expect(watch.inData('b.c.a')).to.be.false;
-		})
-		it('path equal "b.e[2][4]" should return true', () => {
-			expect(watch.inData('b.e[2][4]')).to.be.false;
-		})
-		it('path equal "c[4]" should return true', () => {
-			expect(watch.inData('c[4]')).to.be.false;
-		})
+  describe('inData test', () => {
+    it('path equal "no" should return true', () => {
+      expect(watch.inData('no')).to.be.false;
+    })
+    it('path equal "b.c.a" should return true', () => {
+      expect(watch.inData('b.c.a')).to.be.false;
+    })
+    it('path equal "b.e[2][4]" should return true', () => {
+      expect(watch.inData('b.e[2][4]')).to.be.false;
+    })
+    it('path equal "c[4]" should return true', () => {
+      expect(watch.inData('c[4]')).to.be.false;
+    })
 
-	})
+  })
 })
